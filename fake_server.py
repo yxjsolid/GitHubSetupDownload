8dfb28356a216ebbe7648d5877557bd3eba8fce8
yxjsolid <yxjsolid@gmail.com>
2017-07-18 01:51:06 +0800

    update

--- fake_server.py
+++ fake_server.py
@@ -1,7 +1,8 @@
 from flask import Flask
-app = Flask(__name__, static_folder="download",static_url_path='')
+app = Flask(__name__, static_folder="static",static_url_path='')
 
 if __name__ == "__main__":
     context = ('cert.pem', 'key.pem')
     # app.run(host="10.103.12.31", port=443, debug=1, ssl_context = context)
     app.run(host="127.0.0.1", port=80, debug=1, )
+    # app.run(host="10.103.12.31", port=80, debug=1, )
